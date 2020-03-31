8c307c5cf40d2d71ea3053afdb352fcccdcefcff
Maria Mihai <maria.ic.mihai@gmail.com>
2020-03-31 16:09:01 +0200

    Added Beer repository. Closes #6

---
+++ src/main/java/guru/springframework/sbmbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.sbmbeerservice.repositories;
+
+import guru.springframework.sbmbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
