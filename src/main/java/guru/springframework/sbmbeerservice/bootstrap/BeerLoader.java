784b0c49129729254daaa07445cf4e64e29bb55b
Maria Mihai <maria.ic.mihai@gmail.com>
2020-06-26 13:04:48 +0200

    Modifying the initial beer data - BeerLoader is replaced by the data.sql file. Enabling h2 console access.  Closes #18

--- src/main/java/guru/springframework/sbmbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/springframework/sbmbeerservice/bootstrap/BeerLoader.java
@@ -2,11 +2,13 @@ package guru.springframework.sbmbeerservice.bootstrap;
 
 import guru.springframework.sbmbeerservice.domain.Beer;
 import guru.springframework.sbmbeerservice.repositories.BeerRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
+@Slf4j
 //@Component
 public class BeerLoader implements CommandLineRunner {
 
@@ -28,7 +30,7 @@ public class BeerLoader implements CommandLineRunner {
     // Using data.sql for inserting initial data instead
     private void loadBeerObjects() {
         if(beerRepository.count() == 0) {
-            beerRepository.save(Beer.builder()
+            Beer beer = beerRepository.save(Beer.builder()
                     .beerName("Beer 1")
                     .beerStyle("IPA")
                     .quantityToBrew(200)
@@ -36,17 +38,19 @@ public class BeerLoader implements CommandLineRunner {
                     .upc(BEER_1_UPC)
                     .price(new BigDecimal("11"))
                     .build());
+            log.debug("Saving 'Beer 1' - IPA - with upc and id: " + BEER_1_UPC + "   " + beer.getId());
 
-            beerRepository.save(Beer.builder()
+            beer = beerRepository.save(Beer.builder()
                     .beerName("Beer 2")
-                    .beerStyle("PALE_ALE")
+                    .beerStyle("LAGER")
                     .quantityToBrew(200)
                     .minOnHand(12)
                     .upc(BEER_2_UPC)
                     .price(new BigDecimal("11"))
                     .build());
+            log.debug("Saving 'Beer 2' - LAGER - with upc and id: " + BEER_2_UPC + "   " + beer.getId());
 
-            beerRepository.save(Beer.builder()
+            beer = beerRepository.save(Beer.builder()
                     .beerName("Beer 3")
                     .beerStyle("PALE_ALE")
                     .quantityToBrew(200)
@@ -54,6 +58,7 @@ public class BeerLoader implements CommandLineRunner {
                     .upc(BEER_3_UPC)
                     .price(new BigDecimal("11"))
                     .build());
+            log.debug("Saving 'Beer 3' - PALE_ALE - with upc and id: " + BEER_3_UPC + "   " + beer.getId());
         }
     }
 
