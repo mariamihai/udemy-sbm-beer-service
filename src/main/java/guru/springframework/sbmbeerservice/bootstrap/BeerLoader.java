1e19b2bf6db07f8c6538c61dbd73697bf39a234e
Maria Mihai <maria.ic.mihai@gmail.com>
2020-06-30 16:45:09 +0200

    Add initial beers. Closes #31 - updated the beer loader - updated the hibernate dialect

--- src/main/java/guru/springframework/sbmbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/springframework/sbmbeerservice/bootstrap/BeerLoader.java
@@ -2,6 +2,8 @@ package guru.springframework.sbmbeerservice.bootstrap;
 
 import guru.springframework.sbmbeerservice.domain.Beer;
 import guru.springframework.sbmbeerservice.repositories.BeerRepository;
+import guru.springframework.sbmbeerservice.web.model.BeerStyleEnum;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -9,7 +11,8 @@ import org.springframework.stereotype.Component;
 import java.math.BigDecimal;
 
 @Slf4j
-//@Component
+@RequiredArgsConstructor
+@Component
 public class BeerLoader implements CommandLineRunner {
 
     private final BeerRepository beerRepository;
@@ -18,48 +21,44 @@ public class BeerLoader implements CommandLineRunner {
     public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
 
-    public BeerLoader(BeerRepository beerRepository) {
-        this.beerRepository = beerRepository;
-    }
-
     @Override
     public void run(String... args) {
-        loadBeerObjects();
+        if(beerRepository.count() == 0) {
+            loadBeerObjects();
+        }
     }
 
-    // Using data.sql for inserting initial data instead
     private void loadBeerObjects() {
-        if(beerRepository.count() == 0) {
-            Beer beer = beerRepository.save(Beer.builder()
-                    .beerName("Beer 1")
-                    .beerStyle("IPA")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_1_UPC)
-                    .price(new BigDecimal("11"))
-                    .build());
-            log.debug("Saving 'Beer 1' - IPA - with upc and id: " + BEER_1_UPC + "   " + beer.getId());
+        Beer beer = Beer.builder()
+                .beerName("Mango Bobs")
+                .beerStyle(BeerStyleEnum.IPA.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .price(new BigDecimal("12.95"))
+                .upc(BEER_1_UPC)
+                .build();
+        beerRepository.save(beer);
 
-            beer = beerRepository.save(Beer.builder()
-                    .beerName("Beer 2")
-                    .beerStyle("LAGER")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_2_UPC)
-                    .price(new BigDecimal("11"))
-                    .build());
-            log.debug("Saving 'Beer 2' - LAGER - with upc and id: " + BEER_2_UPC + "   " + beer.getId());
+        beer = Beer.builder()
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyleEnum.PALE_ALE.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .price(new BigDecimal("12.40"))
+                .upc(BEER_2_UPC)
+                .build();
+        beerRepository.save(beer);
 
-            beer = beerRepository.save(Beer.builder()
-                    .beerName("Beer 3")
-                    .beerStyle("PALE_ALE")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_3_UPC)
-                    .price(new BigDecimal("11"))
-                    .build());
-            log.debug("Saving 'Beer 3' - PALE_ALE - with upc and id: " + BEER_3_UPC + "   " + beer.getId());
-        }
+        beer = Beer.builder()
+                .beerName("Pinball Porter")
+                .beerStyle(BeerStyleEnum.PALE_ALE.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .price(new BigDecimal("11.25"))
+                .upc(BEER_3_UPC)
+                .build();
+        beerRepository.save(beer);
+
     }
 
 
