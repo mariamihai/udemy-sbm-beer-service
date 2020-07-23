61c731fbbcd8820a9e315fa1d215dbdecc2d7018
Maria Mihai <maria.ic.mihai@gmail.com>
2020-07-23 20:09:32 +0200

    Remove annotation for listeners + refactoring #18

--- src/main/java/guru/springframework/sbmbeerservice/services/brewing/BrewBeerListener.java
+++ src/main/java/guru/springframework/sbmbeerservice/services/brewing/BrewBeerListener.java
@@ -1,8 +1,6 @@
 package guru.springframework.sbmbeerservice.services.brewing;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.sbmbeerservice.config.JmsConfig;
-import guru.springframework.sbmbeerservice.domain.Beer;
 import guru.springframework.sbmbeerservice.events.BrewBeerEvent;
 import guru.springframework.sbmbeerservice.events.NewInventoryEvent;
 import guru.springframework.sbmbeerservice.repositories.BeerRepository;
@@ -11,9 +9,7 @@ import guru.springframework.sbmbeerservice.web.model.BeerDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,20 +17,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class BrewBeerListener {
 
     private final BeerRepository beerRepository;
-    private final JmsTemplate jmsTemplate;
-    private final ObjectMapper objectMapper;
     private final JmsMessageService jmsMessageService;
 
-    @Transactional
     @JmsListener(destination = JmsConfig.BREWING_REQUEST_QUEUE)
     public void listen(BrewBeerEvent brewBeerEvent) {
         BeerDto beerDto = brewBeerEvent.getBeerDto();
 
-        Beer beer = beerRepository.getOne(beerDto.getId());
-        beerDto.setQuantityOnHand(beer.getQuantityToBrew());
+        beerRepository.findById(beerDto.getId()).ifPresentOrElse(beer -> {
+            beerDto.setQuantityOnHand(beer.getQuantityToBrew());
 
-        log.debug("Brewed " + beer.getQuantityToBrew() + " of beer " + beerDto.getId());
+            log.debug("Brewed " + beer.getQuantityToBrew() + " of beer " + beerDto.getId());
 
-        jmsMessageService.sendJmsMessage(JmsConfig.NEW_INVENTORY_QUEUE, new NewInventoryEvent(beerDto), NewInventoryEvent.class.getSimpleName());
+            jmsMessageService.sendJmsMessage(JmsConfig.NEW_INVENTORY_QUEUE, new NewInventoryEvent(beerDto), NewInventoryEvent.class.getSimpleName());
+
+        }, () -> log.error("Couldn't find beer " + beerDto.getId()));
     }
 }
